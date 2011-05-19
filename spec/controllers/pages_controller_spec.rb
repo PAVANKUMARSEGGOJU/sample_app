7fb1d04befbdfd154950d6900c5fcf2b8c319a44
PAVANKUMARSEGGOJU <pavan@futurenow.biz>
2011-05-19 16:47:36 +0530

    Initial commit

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
 render_views
+before(:each) do
+@base_title = "Ruby on Rails Tutorial Sample App"
+end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +14,7 @@ render_views
 it "should have the right title" do
 get 'home'
 response.should have_selector("title",
-		:content=>"Ruby on Rails tutorial sample App | Home")
+		:content=>"#{@base_title} | Home")
 end
 it "should have a non-blank body" do
 get 'home'
@@ -28,7 +31,7 @@ end
 it "should have the right title" do
 get 'contact'
 response.should have_selector("title",
-		:content=>"Ruby on Rails tutorial sample App | contact")
+		:content=>"#{@base_title} | contact")
 end
 
   end
@@ -40,7 +43,7 @@ describe "GET 'about'" do
 it "should have the right title" do
 get 'about'
 response.should have_selector("title",
-		:content=>"Ruby on Rails tutorial sample App | About")
+		:content=>"#{@base_title} | About")
 end
 
   end
