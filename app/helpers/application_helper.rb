58043ca700134db46fc16d9edc5f7a1eceb7fa26
PAVANKUMARSEGGOJU <pavan@futurenow.biz>
2011-05-19 11:11:30 +0530

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+#Return a title on a per-page basis.
+def titile
+base_title = "Ruby on Rails Tutoril Sample App"
+if @title.nil?
+base_title
+else
+"#{base_title} | #{@title}"
+end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "pages2/pavan1"
+
+  get "pages2/pavan2"
+
   get "pages/home"
 
   get "pages/contact"
+	get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
